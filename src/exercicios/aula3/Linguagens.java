230e1903bd967d95d99ea83d24ea43fc31bec5cb
Jhonata Silva <jhoncsilva28v@gmail.com>
2026-06-02 17:54:31 -0300

    aula 3 não finalizada

---
+++ src/exercicios/aula3/Linguagens.java
@@ -0,0 +1,22 @@
+package exercicios.aula3;
+
+import java.util.ArrayList;
+
+public class Linguagens {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> linguagens = new ArrayList<>();
+		
+		linguagens.add("Java");
+		linguagens.add("Python");
+		linguagens.add("JavaScript");
+		linguagens.add("C++");
+		
+		for (String linguagem : linguagens){
+			System.out.println(linguagem);
+		}
+
+	}
+
+}
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,3 +1,4 @@
+package principal;
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
